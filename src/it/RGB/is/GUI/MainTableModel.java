e1f07a02c96e417f2d69b3bf77c6cc90b77abf50
Edo/windows <edoardo@catlover.com>
2017-06-18 12:26:39 +0200

    Substring lista canzoni

--- src/it/RGB/is/GUI/MainTableModel.java
+++ src/it/RGB/is/GUI/MainTableModel.java
@@ -66,7 +66,10 @@ public class MainTableModel extends AbstractTableModel {
 		case 2:
 			return "<html><h4>" + item.getTitolo();
 		case 3:
-			return "<html><p>" + item.getSongsTitles() + "</p></html>";
+			if(item.getSongsTitles().length() > 150)
+				return "<html><p>" + item.getSongsTitles().substring(0, 150) + "..." + "</p></html>";
+			else
+				return "<html><p>" + item.getSongsTitles() + "</p></html>";
 		case 4:
 			return "<html><center>" + item.getTitolare().getNomeArte() + "</html>";
 		case 5:
