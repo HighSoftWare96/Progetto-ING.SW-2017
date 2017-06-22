8d3bda8e4342554d9d85b09f012a77b094bc6b74
Alberto <alberto.girelli@outlook.it>
2017-06-22 19:08:33 +0200

    [Minor] Piccoli ritocchi a VenditaTest

--- src/it/RGB/is/Tests/Classes/VenditaTest.java
+++ src/it/RGB/is/Tests/Classes/VenditaTest.java
@@ -2,13 +2,11 @@ package it.RGB.is.Tests.Classes;
 
 import static org.junit.Assert.*;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
 
 import it.RGB.is.Classes.Prodotto;
-import it.RGB.is.Classes.Cliente;
 import it.RGB.is.Classes.ModConsegna;
 import it.RGB.is.Classes.Pagamento;
 import it.RGB.is.Classes.Vendita;
@@ -19,53 +17,51 @@ public class VenditaTest {
 	
 	private Vendita venditaTest = null;
 	
-	
+	public VenditaTest() {
+		TestData.initializeData();
+		venditaTest = TestData.getGenericVendita();
+	}
+		
 	@Test (expected = VenditaIllegalArgumentException.class)
 	public void testVenditaClienteNull() throws VenditaIllegalArgumentException {
-		TestData.initializeData();
 		new Vendita(null, new Prodotto[] { TestData.getGenericCd() }, new Integer[] { 20 }, 250, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
 		
 	} 
 	
 	@Test (expected = VenditaIllegalArgumentException.class)
 	public void testVenditaEmptyProduct() throws VenditaIllegalArgumentException {
-		TestData.initializeData();
 		new Vendita(TestData.getGenericCliente(), new Prodotto [] {}, new Integer[] { 20 }, 250, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
 		
 	} 
 	
 	@Test (expected = VenditaIllegalArgumentException.class)
 	public void testVenditaPriceWrong() throws VenditaIllegalArgumentException {
-		TestData.initializeData();
 		new Vendita(TestData.getGenericCliente(), new Prodotto [] { TestData.getGenericCd() }, new Integer[] { 20 }, 0, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
 	}
 	
 	@Test (expected = VenditaIllegalArgumentException.class)
 	public void testVenditaIncorrectAmount() throws VenditaIllegalArgumentException {
-		TestData.initializeData();
 		new Vendita(TestData.getGenericCliente(), new Prodotto [] { TestData.getGenericCd(), TestData.getGenericDVD() }, new Integer[] { 20 }, 250, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
 	}
 	
 	@Test
 	public void testVenditaConstructor() {
-		TestData.initializeData();
-		venditaTest = TestData.getGenericVendita();
 		
+		assertFalse(venditaTest.getProdotti().equals(null));
+		int amount = venditaTest.getAmount().length;
+		assertEquals(1, amount);
+		assertTrue(venditaTest.getPrezzoTotale() == 250);
+		assertTrue(venditaTest.getDateString().length() == 17);
+		Date data = venditaTest.getDate();
+		assertEquals(new Date().toString(), data.toString());
+		String ip = venditaTest.getIP();
+		assertEquals("localhost", ip);	
 		String pagamento = venditaTest.getPagamento();
 		assertEquals("Bonifico bancario", pagamento);
 		String consegna = venditaTest.getConsegna();
 		assertEquals("Corriere 24H", consegna);
 		assertTrue(venditaTest.getProdottiLength() == 1);
-		assertTrue(venditaTest.getPrezzoTotale() == 250);
-		String ip = venditaTest.getIP();
-		assertEquals("localhost", ip);	
-		int amount = venditaTest.getAmount().length;
-		assertEquals(1, amount);
-		Date data = venditaTest.getDate();
-		assertEquals(new Date().toString(), data.toString());
-		assertTrue(venditaTest.getDateString().length() == 17);
-		assertFalse(venditaTest.getProdotti().equals(null));
-		assertEquals("Toto XIV", venditaTest.getProdottoString(0));
-		
+		assertEquals("Toto XIV", venditaTest.getProdottoString(0));	
+
 	}
 }
