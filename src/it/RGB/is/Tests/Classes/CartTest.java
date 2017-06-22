82c972ebe15d72dd2a17405ed785825acd14cc23
Edo/windows <edoardo@catlover.com>
2017-06-22 17:41:57 +0200

    [Test] Cart: aggiunti altri test, in arrivo altri...

--- src/it/RGB/is/Tests/Classes/CartTest.java
+++ src/it/RGB/is/Tests/Classes/CartTest.java
@@ -1,17 +1,43 @@
 package it.RGB.is.Tests.Classes;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import it.RGB.is.Classes.Cart;
+import it.RGB.is.Classes.ModConsegna;
 import it.RGB.is.Exceptions.CartIllegalArgumentsException;
 
 public class CartTest {
+	
+	public CartTest() {
+		TestData.initializeData();
+		Cart.initialize();
+	}
+	
+	@Test
+	public void testCart(){
+		//test rimozione tutti prodotti
+		Cart.addItem(TestData.getGenericCd(), 4);
+		Cart.addItem(TestData.getGenericDVD(), 2);
+		Cart.removeAll();
+		assertTrue(Cart.getCartNumberItems() == 0);
+		
+		//test rimozione un prodotto alla volta
+		Cart.addItem(TestData.getGenericCd(), 2);
+		Cart.removeItem(TestData.getGenericCd(), 1);
+		Cart.removeItem(TestData.getGenericCd(), 1);
+		
+		assertTrue(Cart.getCartNumberItems() == 0);
+		
+		Cart.addItem(TestData.getGenericCd(), 1);
+		assertTrue((float) Cart.getSubTotale(ModConsegna.CORRIERE_24H) != (float) 2);
+		
+	}
 
 	@Test(expected = CartIllegalArgumentsException.class) 
 	public void removeItemNull() {
 
-		
-	     Cart.initialize();
 	     Cart.removeItem(null, 1);
 	    
 	}
@@ -19,8 +45,6 @@ public class CartTest {
 	@Test(expected = CartIllegalArgumentsException.class) 
 	public void removeItemAmount() {
 
-		
-	     Cart.initialize();
 	     Cart.removeItem(TestData.getGenericCd(), -1);
 	     Cart.removeItem(TestData.getGenericCd(), 0);
 	}
@@ -28,16 +52,12 @@ public class CartTest {
 	@Test(expected = CartIllegalArgumentsException.class) 
 	public void addItemNull() {
 
-		
-	     Cart.initialize();
 	     Cart.addItem(null, 1);
 	    
 	}
 	@Test(expected = CartIllegalArgumentsException.class) 
 	public void addItemAmount() {
 
-		
-	     Cart.initialize();
 	     Cart.addItem(TestData.getGenericCd(), -1);
 	     Cart.addItem(TestData.getGenericCd(), 0);
 	     Cart.addItem(TestData.getGenericCd(), 110);
