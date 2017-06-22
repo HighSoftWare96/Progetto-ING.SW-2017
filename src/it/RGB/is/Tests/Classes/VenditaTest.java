657e72088b45d8b4c7403cd8ae5efe830a833885
Alberto <alberto.girelli@outlook.it>
2017-06-22 16:16:23 +0200

    VenditaTest funzionante quasi concluso

--- src/it/RGB/is/Tests/Classes/VenditaTest.java
+++ src/it/RGB/is/Tests/Classes/VenditaTest.java
@@ -7,6 +7,9 @@ import java.util.Date;
 
 import org.junit.Test;
 
+import it.RGB.is.Classes.Prodotto;
+import it.RGB.is.Classes.ModConsegna;
+import it.RGB.is.Classes.Pagamento;
 import it.RGB.is.Classes.Vendita;
 import it.RGB.is.Tests.Classes.TestData;
 import it.RGB.is.Exceptions.VenditaIllegalArgumentException;
@@ -14,13 +17,21 @@ import it.RGB.is.Exceptions.VenditaIllegalArgumentException;
 public class VenditaTest {
 	
 	private Vendita venditaTest;
-
+	
+	
 	@Test (expected = VenditaIllegalArgumentException.class)
-	public void testVenditaNull() {
+	public void testVenditaClienteNull() throws VenditaIllegalArgumentException {
 		TestData.initializeData();
-		new Vendita(null,null, null, 0, new Date(), "", null, null);
+		venditaTest = new Vendita(null, new Prodotto[] { TestData.getGenericCd() }, new Integer[] { 20 }, 250, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
 		
-	}
+	} 
+	
+	@Test (expected = VenditaIllegalArgumentException.class)
+	public void testVenditaEmpty() throws VenditaIllegalArgumentException {
+		TestData.initializeData();
+		new Vendita(TestData.getGenericCliente(), new Prodotto [] {}, new Integer[] {20}, 250, new Date(), "localhost", Pagamento.BONIFICO, ModConsegna.CORRIERE_24H);
+		
+	} 
 	
 	@Test
 	public void testVenditaConstructor() {
@@ -31,12 +42,18 @@ public class VenditaTest {
 		assertEquals("Bonifico bancario", pagamento);
 		String consegna = venditaTest.getConsegna();
 		assertEquals("Corriere 24H", consegna);
-		assertTrue(venditaTest.getProdottiLength() == 20);
+		assertTrue(venditaTest.getProdottiLength() == 1);
 		assertTrue(venditaTest.getPrezzoTotale() == 250);
 		String ip = venditaTest.getIP();
-		assertEquals("localhost", ip);
-		Date data = venditaTest.getDate();
-		assertEquals(new Date(), data);
-			
+		assertEquals("localhost", ip);		
+		
+		
+		//TODO
+		//.getAmount()
+		//.getProdottoString(int index)
+		//.getDate()
+		//.getDateString()
+		//.getProdotto()
+
 	}
 }
