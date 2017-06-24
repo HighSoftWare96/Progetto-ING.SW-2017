f7aba3622c9634ba45fe49c4e128361fd7986872
HighSoftWare96 <bertonc96@gmail.com>
2017-06-24 15:25:19 +0200

    [Minor]

--- src/it/RGB/is/Exceptions/LightBancaVenditeException.java
+++ src/it/RGB/is/Exceptions/LightBancaVenditeException.java
@@ -1,6 +1,6 @@
 package it.RGB.is.Exceptions;
 
-public class LightBancaVenditeException extends Exception {
+public class LightBancaVenditeException extends IllegalArgumentException {
 	
 	private static final long serialVersionUID = 1L;
 
