69474debe4cc08b48d08ad0e3784dd053e7a5bc1
Edo/windows <edoardo@catlover.com>
2017-06-24 10:06:03 +0200

    Divisione Test CartTest

--- src/it/RGB/is/Tests/Integration/CartTest.java
+++ src/it/RGB/is/Tests/Integration/CartTest.java
@@ -16,16 +16,19 @@ public class CartTest {
 		TestData.initializeData();
 		Cart.initialize();
 	}
-
+	
 	@Test
-	public void testCart() {
-
+	public void removeAllTest(){
 		// test rimozione tutti prodotti
 		Cart.addItem(TestData.getGenericCd(), 4);
 		Cart.addItem(TestData.getGenericDVD(), 2);
 		Cart.removeAll();
 		assertTrue(Cart.getCartNumberItems() == 0);
-
+	}
+	
+	@Test
+	public void removeTest(){
+		Cart.removeAll();
 		// test rimozione un prodotto alla volta
 		Cart.addItem(TestData.getGenericCd(), 2);
 		Cart.removeItem(TestData.getGenericCd(), 1);
@@ -35,45 +38,74 @@ public class CartTest {
 		Cart.removeItem(TestData.getGenericCd(), 1);
 		// carrello vuoto
 		assertTrue(Cart.getCartNumberItems() == 0);
-
+	}
+	
+	@Test
+	public void calcPriceTest(){
+		Cart.removeAll();
 		Cart.addItem(TestData.getGenericCd(), 1);
 
 		// calcolo prezzo
 		assertTrue((float) Cart.calculateSubTotaleNotSped() == (float) 12.23);
 		assertTrue((float) Cart.getSubTotale(ModConsegna.CORRIERE_24H) != (float) 2);
 		assertTrue((float) Cart.getSubTotale(ModConsegna.CORRIERE_24H) == (float) 19.23);
-
+	}
+	
+	public void productsInCartTest(){
+		Cart.removeAll();
+		Cart.addItem(TestData.getGenericCd(), 1);
+		
 		// numero prodotti nel carrello
 		assertTrue(Cart.getCartNumberItems() == 1);
 		assertTrue(Cart.getCart().length == 1);
-
+	}
+	
+	@Test
+	public void IDFromProductTest(){
+		Cart.removeAll();
+		Cart.addItem(TestData.getGenericCd(), 1);
+		
 		// ID da prodotto
 		assertTrue(Cart.getQuantita(TestData.getGenericCd().getID()) == 1);
+	}
 
+	@Test
+	public void addToExistingTest(){
+		Cart.removeAll();
+		Cart.addItem(TestData.getGenericCd(), 1);
+		
 		// aumento quantità di un prodotto già presente
 		Cart.addItem(TestData.getGenericCd(), 1);
 		assertTrue(Cart.getQuantita(TestData.getGenericCd().getID()) == 2);
-
+	}
+	
+	@Test
+	public void productFromIDTest(){
+		Cart.removeAll();
+		Cart.addItem(TestData.getGenericCd(), 1);
+		
 		// Prodotto da ID
 		assertTrue(Cart.getItemByID(TestData.getGenericCd().getID()).equals(TestData.getGenericCd()));
-		Cart.removeItem(TestData.getGenericCd(), 1);
-		Cart.removeItem(TestData.getGenericCd(), 1);
-
-		// numero prodotti nel carrello
-		assertTrue(Cart.getCartNumberItems() == 0);
-
+	}
+	
+	@Test
+	public void completeTest(){
+		Cart.removeAll();
 		// completamento acquisto
 		Cart.addItem(TestData.getGenericCd(), 1);
 		Cart.addItem(TestData.getGenericCd(), 1);
 		Cart.buyAndRemove();
 		assertTrue(Cart.getCartNumberItems() == 0);
-
+	}
+	
+	@Test
+	public void amountLengthTest() {
+		Cart.removeAll();
 		// get amount length
 		Cart.addItem(TestData.getGenericCd(), 1);
 		Cart.addItem(TestData.getGenericCd(), 1);
 		assertTrue(Cart.getAmount().length == Cart.getCartNumberItems());
 		assertTrue(Cart.getAmount().length == Cart.getCart().length);
-
 	}
 
 	@Test(expected = CartIllegalArgumentsException.class)
