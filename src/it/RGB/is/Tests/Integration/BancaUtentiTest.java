1f5f9652e5cb8ae6d5c37517884ca2a7587bce8b
HighSoftWare96 <bertonc96@gmail.com>
2017-06-29 22:37:50 +0200

    BancaUtenti Test corretto
    Il try catch copriva l'eccezione per cui il cliente era inesistente nella banca

--- src/it/RGB/is/Tests/Integration/BancaUtentiTest.java
+++ src/it/RGB/is/Tests/Integration/BancaUtentiTest.java
@@ -15,17 +15,17 @@ import it.RGB.is.GUI.Controller;
 import it.RGB.is.Tests.Unit.TestData;
 
 public class BancaUtentiTest {
-	
-	private void newUser(){
+
+	private void newUser() {
 		try {
-			Cliente user = new Cliente("RGHDRD96D15B296T", "user", "password", "User", "Test",
-					"City", "12312312364", null);
+			Cliente user = new Cliente("RGHDRD96D15B296T", "user", "password", "User", "Test", "City", "12312312364",
+					null);
 			BancaUtenti.addItem(user);
 		} catch (Exception e) {
 		}
 	}
 
-	@Test (expected = BancaUtentiIllegalArgumentException.class)
+	@Test(expected = BancaUtentiIllegalArgumentException.class)
 	public void testAddItem() throws BancaUtentiIllegalArgumentException {
 		BancaUtenti.addItem(null);
 	}
@@ -41,28 +41,29 @@ public class BancaUtentiTest {
 
 	@Test
 	public void testSetLoggedOut() {
-		
+
 		TestData.initializeData();
 		BancaUtenti.setLoggedOut();
 		assertTrue(BancaUtenti.getLoggedInUser() == null);
 	}
 
 	@Test
-	public void testGetLoggedInUser() {
-		
+	public void testGetLoggedInUser() throws BancaUtentiIllegalArgumentException {
+
 		TestData.initializeData();
-		try{
+
+		try {
+			BancaUtenti.addItem(TestData.getGenericCliente());
 			Controller.setLoggedIn(TestData.getGenericCliente());
-		
 			assertTrue(BancaUtenti.getLoggedInUser().equals(TestData.getGenericCliente()));
-		
-		} catch (NullPointerException e){
+		} catch (NullPointerException e) {
+			System.out.println("Dovuto all'interfaccia non esistente");
 		}
 	}
 
 	@Test
 	public void testUserNameExists() {
-		
+
 		TestData.initializeData();
 		newUser();
 		assertTrue(BancaUtenti.userNameExists("user"));
